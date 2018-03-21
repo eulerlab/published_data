b8799d9c3edfbdc21276be54b770df02f0485dfb
Thomas Euler <thomas.euler@cin.uni-tuebingen.de>
2018-03-21 14:23:10 +0100

    Update

--- scripts/h5py_helper.py
+++ scripts/h5py_helper.py
@@ -20,33 +20,54 @@ def nan2zero(data):
     
 # ---------------------------------------------------------------------
 preLev = 0
+curLev = 0
+nLine  = 0
+sNew   = ""
+sPrev  = ""
 
 def __print(path, obj):
-    global preLev
+    global preLev, curLev, nLine, sNew, sPrev
     
+    preLev = curLev
     curLev = path.count("/")
     minInd = "   "
     indStr = minInd *curLev
+    sNew   = ""
     if isinstance(obj, h5py.Group):
-        print("{0}{1}".format(indStr, obj.name))
+        sNew = "{0}{1}".format(indStr, obj.name.split("/")[-1])
         
     elif isinstance(obj, h5py.Dataset):    
         dataSetName = obj.name.split("/")[-1]
         if dataSetName == "_info":
             text = obj[()].decode("utf-8")
-            print("{0}|  info='{1}'".format(indStr, text))
+            sNew = "{0}|  info='{1}'".format(indStr, text)
         else:
-            print("{0}|--{1}{2}".format(indStr, dataSetName, obj.shape))
+            sNew = "{0}|--{1}{2}".format(indStr, dataSetName, obj.shape)
         
     if len(obj.attrs) > 0:
         text = obj.attrs["desc"][0].decode("utf-8")
-        print("{0}desc='{1}'".format(indStr +minInd, text))
+        sNew = sNew +"\r{0}desc='{1}'".format(indStr +minInd, text)
+        
+    if nLine > 0:
+        if (curLev < preLev):
+            sPrev = sPrev.replace("|", "`")
+        print(sPrev)
+    sPrev = sNew
+    nLine += 1
+        
     
 def print_hfd5_directory(path):    
-    global preLev 
+    """ Prints a simple directory of the file
+    """
+    global preLev, nLine, sPrev, sNew 
     
-    preLev = 0    
+    preLev = 0
+    curLev = 0
+    nLine  = 0
+    sNew   = ""
+    sPrev  = ""
     f = h5py.File(path, 'r')
     f.visititems(__print)
+    print(sNew.replace("|", "`"))
     
 # ---------------------------------------------------------------------
